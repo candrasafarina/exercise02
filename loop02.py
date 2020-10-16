9fea74490df615a5b1ca8bb7b1a9c809f93a1135
candrasafarina <candrasafarina@gmail.com>
2020-10-16 16:05:57 +0700

    belajar loop

---
+++ loop02.py
@@ -0,0 +1,28 @@
+# prints out the numbers 0,1,2,3,4,
+
+for x in range(5):
+    print(x)
+    
+    
+    
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+    
+count = 0
+while count < 7:
+    print(count)
+    count += 1
+    
+# Break
+# Printe out 0,1,2,3,4
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+    
+    
+    
