71344f8c214946af391f6b6b01feaa7b1374ae44
candrasafarina <candrasafarina@gmail.com>
2020-10-19 10:32:00 +0700

    latihan senin 19/10/2020

---
+++ latihan03/latihan03.py
@@ -0,0 +1,12 @@
+for x in range ( 1, 11):
+    if x%2 == 1:
+        continue
+    print(x)
+
+for x in range (11):
+    if x %2 == 1:
+        print(x)
+
+for x in range ( 1, 11):
+    if x%2 == 1:
+        print(x)
